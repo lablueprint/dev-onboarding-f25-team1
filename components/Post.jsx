8ddd1b59b393f3203c2b077743d722e6dd84f9b9
katieyungchung <katieyungchung@g.ucla.edu>
2025-11-10 01:00:23 -0800

    add comment icon and input textbox

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,10 +1,12 @@
+import { IconMessagePlus } from '@tabler/icons-react-native';
 import { useState } from 'react';
-import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const bookmarkedImage = require('../assets/images/post_bookmarked.png');
 const notBookmarkedImage = require('../assets/images/post_not_bookmarked.png');
 export default function Post() {
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [text, setText] = useState('');
 
   function handleBookmarkPress() {
     setIsBookmarked(!isBookmarked);
@@ -19,12 +21,22 @@ export default function Post() {
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.container}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text>This is one post</Text>
+          <View style={styles.postRow}>
+            <Text>This is one example post</Text>
             <Pressable style={styles.bookmarkButton} onPress={handleBookmarkPress}>
               <Image source={bookmarkIconSource} style={styles.image} />
             </Pressable>
           </View>
+          <View style={styles.commentContainer}>
+              <IconMessagePlus/>
+              <TextInput
+                  style={styles.input}
+                  placeholder="Write a comment..."
+                  value={text}
+                  onChangeText={setText}
+                  multiline
+              />
+            </View>
         </View>
     </ScrollView>
   );
@@ -42,6 +54,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'black'
   },
+  postRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
   bookmarkButton: {
     marginLeft: 10,
   },
@@ -49,5 +66,19 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
     alignSelf: 'center',
+  },
+  commentContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  input: {
+    flex: 1,
+    fontSize: 14,
+    marginLeft: 8,
   },
 });
