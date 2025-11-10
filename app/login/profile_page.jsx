53165d1ae6abc7efda23eeadc2d825268c2121bc
Gokul Nambiar <gokulnambiar@Gokuls-MacBook-Air.local>
2025-11-09 23:21:45 -0800

    set up starting file structure

---
+++ app/login/profile_page.jsx
@@ -0,0 +1,15 @@
+import {View, ScrollView} from 'react-native';
+
+export default function Profile {
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+    <View style={styles.container}></View>
+{/* 
+    <Text style = {styles.subtitle}>
+    </Text>  */}
+
+  );
+}
+
+
+const styles = Style
