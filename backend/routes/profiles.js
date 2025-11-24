8ad221f4b0ca6034e625008f29fe79fb9b84cef5
Victoria Choi <121906670+vicchois@users.noreply.github.com>
2025-11-24 01:04:30 -0800

    merge main

---
+++ backend/routes/profiles.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {
+  getUserInfo,
+} = require('../controllers/profileController');
+
+const router = express.Router();
+
+//general routes
+router.get('/:username', getUserInfo);
+
+module.exports = router;
